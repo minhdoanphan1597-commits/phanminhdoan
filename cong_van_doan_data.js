b46ee7984cf1b8a63c0cab89084b286760233029
minhdoanphan1597-commits <minhdoanphan1597@gmail.com>
2026-04-24 10:32:42 +0700

    Add files via upload

---
+++ cong_van_doan_data.js
@@ -0,0 +1,53 @@
+window.CVD_DOAN_DATA = {
+  "fetchTime": "21/04/2026 17:00:00",
+  "totalDocs": 4,
+  "soQuaHan": 0,
+  "soGanHan": 0,
+  "soChuaXem": 4,
+  "docs": [
+    {
+      "soHieu": "12-KH/ĐTN",
+      "loai": "Kế Hoạch",
+      "trichYeu": "Kế hoạch Tổ chức các hoạt động chào mừng kỷ niệm 95 năm Ngày thành lập Đoàn TNCS Hồ Chí Minh",
+      "noiGui": "Ban Thường vụ",
+      "ngayDen": "20/03/2026",
+      "hanXuLy": "",
+      "link": "#",
+      "status": "chua_xem",
+      "daysLeft": null
+    },
+    {
+      "soHieu": "45-QĐ/ĐTN",
+      "loai": "Quyết định",
+      "trichYeu": "Quyết định chuẩn y danh sách Đoàn viên ưu tú giới thiệu cho Đảng xem xét kết nạp đợt 1 năm 2026",
+      "noiGui": "Ban Chấp Hành",
+      "ngayDen": "15/03/2026",
+      "hanXuLy": "",
+      "link": "#",
+      "status": "chua_xem",
+      "daysLeft": null
+    },
+    {
+      "soHieu": "88/CV-ĐTN",
+      "loai": "Công văn",
+      "trichYeu": "V/v tăng cường tuyên truyền, vận động đoàn viên thanh niên tham gia Cuộc thi Bàn tay vàng cấp Nước",
+      "noiGui": "Đoàn Khối",
+      "ngayDen": "12/03/2026",
+      "hanXuLy": "",
+      "link": "#",
+      "status": "chua_xem",
+      "daysLeft": null
+    },
+    {
+      "soHieu": "24-TT/ĐTN",
+      "loai": "Thông tri",
+      "trichYeu": "Thông tri triệu tập Hội nghị Ban chấp hành Đoàn thanh niên Công ty mở rộng sơ kết Quý 1 năm 2026",
+      "noiGui": "Ban Chấp Hành",
+      "ngayDen": "05/03/2026",
+      "hanXuLy": "",
+      "link": "#",
+      "status": "chua_xem",
+      "daysLeft": null
+    }
+  ]
+};
